99cbed9deb95a637870aab1ac507712797853009
Faraz Firouzi <73736063+FarazFirouzi@users.noreply.github.com>
2023-02-07 04:48:11 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Monday-Week-1-FarazFirouziCS/main.cpp
@@ -0,0 +1,24 @@
+// Monday-Week-1
+// Due: Friday, February 10, 11:59 PM
+/*************************************************
+Student First Name: Faraz  
+Student Last Name: Firouzi
+**************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string FirstName, LastName;
+
+int main() {
+cout << "what is your name and last name?";
+cin >> FirstName >> LastName;
+cout << "Hi, " << FirstName << " "<< LastName << endl;
+
+}
+
+/*********************************************
+// Output:
+
+**********************************************/
